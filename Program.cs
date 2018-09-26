a049d0c47bcfe25312424bafc7892fbeadc9889d
Daniel Hunter <dan.hunter55@yahoo.com>
2018-09-26 02:10:20 +0100

    Files Uploaded

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SelfTrainingProject.InterviewExcesize.ReversingAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // entered backwards string that will show in reverse and be readable on the console.
+            string StringExample = "noitartsnomed lasrever gnirts rof sdrow fo gnirts gnoL";
+
+            char[] ChararrayExample = StringExample.ToCharArray();
+            Array.Reverse(ChararrayExample);
+            string ReversedStringExample = new string(ChararrayExample);
+
+            Console.Write(ReversedStringExample);
+        }
+    }
+}
